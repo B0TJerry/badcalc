6d2668477f8643f923c734267ffa3310413762ff
Jerry Yurek <81500962+B0TJerry@users.noreply.github.com>
2021-04-08 11:55:42 -0400

    added else statement and other things
    I made it so that if the user inputs anything else than 1-5, the program will tell the user to use a valid calculator action. in addition to that I discovered how to make it so that I don't have to do std:: before a lot of statements.

--- badcalc.cpp
+++ badcalc.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
-int main()
-{
+#include <cmath>
+using namespace std;
+int main() {
     int calcmode;
     double num1 = 0;
     double num2 = 0;
-    std::cout << "Please specify what you want to do:\n--------------------------------------\n|1.Multiply|2.Divide|3.Add|4.Subtract|\n--------------------------------------\n";
-    std::cin >> calcmode;
+    cout << "Please specify what you want to do:\n---------------------------------------------------\n|1.Multiply|2.Divide|3.Add|4.Subtract|5.SquareRoot|\n---------------------------------------------------\n";
+    cin >> calcmode;
     if (calcmode == 1) {
-        std::cout << "Please enter two numbers to multiply by together.\n";
-        std::cin >> num1 >> num2;
-        std::cout << num1 << " * " << num2 << " = " << num1*num2 << ".\n";}
+        cout << "Please enter two numbers to multiply by together.\n";
+        cin >> num1 >> num2;
+        cout << num1 << " * " << num2 << " = " << num1*num2 << ".\n";}
     if (calcmode == 2) {
-        std::cout << "Please enter two numbers to divide by eachother by.\n";
-        std::cin >> num1 >> num2;
-        std::cout << num1 << " / " << num2 << " = " << num1/num2 << ".\n";}
+        cout << "Please enter two numbers to divide by eachother by.\n";
+        cin >> num1 >> num2;
+        cout << num1 << " / " << num2 << " = " << num1/num2 << ".\n";}
     if (calcmode == 3) {
-        std::cout << "Please two numbers to add to eachother.\n";
-        std::cin >> num1 >> num2;
-        std::cout << num1 << " + " << num2 << " = " << num1+num2 << ".\n";}
+        cout << "Please two numbers to add to eachother.\n";
+        cin >> num1 >> num2;
+        cout << num1 << " + " << num2 << " = " << num1+num2 << ".\n";}
     if (calcmode == 4) {
-        std::cout << "Please enter two numbers to subtract by eachother.\n";
-        std::cin >> num1 >> num2;
-        std::cout << num1 << " - " << num2 << " = " << num1-num2 << ".\n";}}
+        cout << "Please enter two numbers to subtract by eachother.\n";
+        cin >> num1 >> num2;
+        cout << num1 << " - " << num2 << " = " << num1-num2 << ".\n";}
+    if (calcmode == 5) {
+        cout << "Please enter a number to get the square root of\n";
+        cin >> num1;
+        cout << "The Square Root of " << num1 << " is " << sqrt(num1) << "\n";}
+    else {
+        cout << "Please enter a valid calculator action.\n";}                                           
+    return 0;
+}
