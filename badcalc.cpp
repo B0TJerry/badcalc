b11d781ad90a8f820989710a96ed661ccce30c6f
Jerry Yurek <81500962+B0TJerry@users.noreply.github.com>
2021-04-08 13:59:57 -0400

    added logo, removed some text, added exponent
    I made a logo using https://textkool.com/en/test-ascii-art-generator?text=badcalc, I eventually decided to remove part of the prompt that said along the lines of please choose a calculator type. I thought having that under the logo looked kinda bad and I didn't care enough about it to put it centered or around the displayed calculator modes. In addition to that I wanted to add the ability to calculate exponents to the calculator, as you can see I add that as well.

--- badcalc.cpp
+++ badcalc.cpp
@@ -5,7 +5,13 @@ int main() {
     int calcmode;
     double num1 = 0;
     double num2 = 0;
-    cout << "Please specify what you want to do:\n---------------------------------------------------\n|1.Multiply|2.Divide|3.Add|4.Subtract|5.SquareRoot|\n---------------------------------------------------\n";
+    cout << R"(
+ ______   ______   _____    ______  ______   _        ______ 
+| |  | \ | |  | | | | \ \  | |     | |  | | | |      | |     
+| |--| < | |__| | | |  | | | |     | |__| | | |   _  | |     
+|_|__|_/ |_|  |_| |_|_/_/  |_|____ |_|  |_| |_|__|_| |_|____ 
+    )" << '\n';
+    cout << "--------------------------------------------------------------\n|1.Multiply|2.Divide|3.Add|4.Subtract|5.SquareRoot|6.Exponent|\n--------------------------------------------------------------\n";
     cin >> calcmode;
     if (calcmode == 1) {
         cout << "Please enter two numbers to multiply by together.\n";
@@ -16,7 +22,7 @@ int main() {
         cin >> num1 >> num2;
         cout << num1 << " / " << num2 << " = " << num1/num2 << ".\n";}
     if (calcmode == 3) {
-        cout << "Please two numbers to add to eachother.\n";
+        cout << "Please enter two numbers to add to eachother.\n";
         cin >> num1 >> num2;
         cout << num1 << " + " << num2 << " = " << num1+num2 << ".\n";}
     if (calcmode == 4) {
@@ -27,7 +33,10 @@ int main() {
         cout << "Please enter a number to get the square root of\n";
         cin >> num1;
         cout << "The Square Root of " << num1 << " is " << sqrt(num1) << "\n";}
+    if (calcmode == 6) {
+        cout << "Please enter the base and exponent.\n";
+        cin >> num1 >> num2;
+        cout << num1 << " to the power of " << num2 << " is " << pow(num1, num2) << ".\n";}
     else {
         cout << "Please enter a valid calculator action.\n";}                                           
-    return 0;
-}
+    return 0;}
