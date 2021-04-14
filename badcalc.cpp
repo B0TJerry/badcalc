535fcba2e096adc633dc8a9eca3acc93082ae100
Jerry Yurek <81500962+B0TJerry@users.noreply.github.com>
2021-04-14 10:27:45 -0400

    Update badcalc.cpp

--- badcalc.cpp
+++ badcalc.cpp
@@ -13,26 +13,26 @@ int main() {
         cout << "Please enter two numbers to multiply by together.\n";
         cin >> num1 >> num2;          //reads 1 floating point values into num1 and num2
         cout << num1 << " * " << num2 << " = " << num1*num2 << "\n";}  //instead of using another floating point value and namin it ans I just took out the ans and just made it solve the problem in the answer statement  
-    if (calcmode == 2) {
+    else if (calcmode == 2) {
         cout << "Please enter two numbers to divide by eachother by.\n";
         cin >> num1 >> num2;          //reads 1 floating point values into num1 and num2 each
         cout << num1 << " / " << num2 << " = " << num1/num2 << "\n";}
-    if (calcmode == 3) {
+    else if (calcmode == 3) {
         cout << "Please enter two numbers to add to eachother.\n";
         cin >> num1 >> num2;          //reads 1 floating point value into num1 and num2 each
         cout << num1 << " + " << num2 << " = " << num1+num2 << "\n";}
-    if (calcmode == 4) {
+    else if (calcmode == 4) {
         cout << "Please enter two numbers to subtract from eachother.\n";
         cin >> num1 >> num2;          //reads 1 floating point values into num1 and num2 each
         cout << num1 << " - " << num2 << " = " << num1-num2 << "\n";}
-    if (calcmode == 5) {
+    else if (calcmode == 5) {
         cout << "Please enter a number to get the square root of.\n";
         cin >> num1;                  //reads 1 floating point value into num1
         cout << "The Square Root of " << num1 << " is " << sqrt(num1) << ".\n";}
-    if (calcmode == 6) {
+    else if (calcmode == 6) {
         cout << "Please enter the base and exponent.\n";
         cin >> num1 >> num2;          //reads 1 floating point value into num1 and num2 each
         cout << num1 << " ^ " << num2 << " = " << pow(num1, num2) << "\n";}
-    else if (calcmode > 6 || calcmode < 1) {
+    else {
         cout << "Please enter a valid calculator action.\n";}    //puts out text telling them to enter a valid mode if they didn't type a valid number, not sure if this is a good way to do it.                                      
     return 0;}
