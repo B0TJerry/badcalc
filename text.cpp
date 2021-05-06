6f8e00eefa846c8f62d6b5e63732c8dd8a017295
Jerry Yurek <81500962+B0TJerry@users.noreply.github.com>
2021-05-05 22:54:32 -0400

    Update text.cpp

--- text.cpp
+++ text.cpp
@@ -1,6 +1,20 @@
 #include <string>
 using namespace std;
-std::string menu = "----------------------------------------------------------------------------\n|0.EXit|1.meNu|2.Multiply|3.Divide|4.Add|5.Subtract|6.SQuareroot|7.Exponent|\n----------------------------------------------------------------------------\n|8.GEometricsrs+seq|9.Probability|\n----------------------------------\n";
-std::string valcalc = "-----------------------------------------\n|Please enter a valid calculator action.|\n-----------------------------------------\n";
-std::string actualnum = "-------------------------------\n|Please enter an acutal number|\n-------------------------------\n";
-std::string whatelse = "---------------------------------\n|What else would you like to do?|\n---------------------------------\n";
+string menu = "----------------------------------------------------------------------------\n|0.EXit|1.meNu|2.Multiply|3.Divide|4.Add|5.Subtract|6.SQuareroot|7.Exponent|\n----------------------------------------------------------------------------\n|8.GEometricsrs+seq|9.Probability|\n----------------------------------\n";
+string valcalc = "-----------------------------------------\n|Please enter a valid calculator action.|\n-----------------------------------------\n";
+string actualnum = "-------------------------------\n|Please enter an acutal number|\n-------------------------------\n";
+string whatelse = "---------------------------------\n|What else would you like to do?|\n---------------------------------\n";
+string bye = "----------\n|Goodbye!|\n----------\n";
+string multiply = "---------------------------------------------------\n|Please enter two numbers to multiply by together.|\n---------------------------------------------------\n";
+string divide = "-----------------------------------------------------\n|Please enter two numbers to divide by eachother by.|\n-----------------------------------------------------\n";
+string add = "-----------------------------------------------\n|Please enter two numbers to add to eachother.|\n-----------------------------------------------\n";
+string subtract = "------------------------------------------------------\n|Please enter two numbers to subtract from eachother.|\n------------------------------------------------------\n";
+string squareroot  = "--------------------------------------------------\n|Please enter a number to get the square root of.|\n--------------------------------------------------\n";
+string exponent = "-------------------------------------------------\n|Please enter the base First, then the exponent.|\n-------------------------------------------------\n";
+string probmenu = "----------------------------\n|0.Back|1.P(A|B)|2.p(aandB)|\n----------------------------\n";
+string pab = "----------------------------------------\n|Please enter P(AandB) followed by P(B)|\n----------------------------------------\n";
+string paandb = "------------------------------------\n|Please enter P(A) followed by P(B)|\n------------------------------------\n";
+string geomenu = "------------------------------------------------------\n|0.Back|1.sUmoffinitegsrs|2.Geoseqrule|3.sumofInfgsrs|\n------------------------------------------------------\n";
+string sumoffinite = "---------------------------\n|Please enter R, A1, and N|\n---------------------------\n";
+string geoseqrule = "------------------------------------------------------------\n|Please enter R and An with its subscript sepreated from it|\n------------------------------------------------------------\n";
+string sumofinf = "-----------------------\n|please enter R and A1|\n-----------------------\n";
