ed7417d9c8b9ab21330209337877f27ead3c41e3
Jerry Yurek <81500962+B0TJerry@users.noreply.github.com>
2021-05-05 20:53:32 -0400

    added a file
    made a file containing any of the longer strings to make the code leaner, It is just more efficient to have a string to reference from another file rather than just the same text repeated needlessly

---
+++ text.cpp
@@ -0,0 +1,6 @@
+#include <string>
+using namespace std;
+std::string menu = "----------------------------------------------------------------------------\n|0.EXit|1.meNu|2.Multiply|3.Divide|4.Add|5.Subtract|6.SQuareroot|7.Exponent|\n----------------------------------------------------------------------------\n|8.GEometricsrs+seq|9.Probability|\n----------------------------------\n";
+std::string valcalc = "-----------------------------------------\n|Please enter a valid calculator action.|\n-----------------------------------------\n";
+std::string actualnum = "-------------------------------\n|Please enter an acutal number|\n-------------------------------\n";
+std::string whatelse = "---------------------------------\n|What else would you like to do?|\n---------------------------------\n";
